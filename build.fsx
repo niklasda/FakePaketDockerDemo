c6c8aa75beb250d9b28349eb15e14320e9640a43
Niklas Dahlman <nd@sql8r.net>
2016-03-10 14:37:40 +0100

    cleaning

--- build.fsx
+++ build.fsx
@@ -1,11 +1,40 @@
+#indent "off"
+
 // include Fake lib
 #r @"packages\FAKE\tools\FakeLib.dll"
 open Fake
 
-// Default target
-Target "Default" (fun _ ->
-    trace "Hello World from FAKE"
+
+// Properties
+let buildDir = "./build/";
+let testDir  = "./test/";
+
+// Targets
+Target "Clean" (fun _ ->
+	CleanDirs [buildDir; testDir]
 )
 
+Target "BuildApp" (fun _ ->
+   !! "src/app/**/*.csproj"
+	 |> MSBuildRelease buildDir "Build"
+	 |> Log "AppBuild-Output: "
+)
+
+Target "BuildTest" (fun _ ->
+	!! "src/test/**/*.csproj"
+	  |> MSBuildDebug testDir "Build"
+	  |> Log "TestBuild-Output: "
+)
+
+Target "Default" (fun _ ->
+	trace "Hello World from FAKE"
+)
+
+// Dependencies
+"Clean"
+  ==> "BuildApp"
+  ==> "BuildTest"
+  ==> "Default"
+
 // start build
-RunTargetOrDefault "Default"
+RunTargetOrDefault "Default";
